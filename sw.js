e31d3508ce2ed96ee0ec0bb915acb8a289deb27a
JuGuz <70982213+JuGuz@users.noreply.github.com>
2025-11-16 18:31:21 +0000

    Initial commit

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'entrena-pro-v1';
+const urls = ['/','/index.html','/manifest.json','https://cdn.tailwindcss.com'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
